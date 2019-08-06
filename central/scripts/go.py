dc3a699c65807249cdf6c4eb052c7fce521e2234
sean-hackett <seanhackett55@gmail.com>
2019-08-06 10:29:14 -0400

    Clean up go.py output

--- central/scripts/go.py
+++ central/scripts/go.py
@@ -6,14 +6,15 @@ def main():
 	rospy.init_node('central')
 
 	# Start time
-	tp = rospy.Publisher('~start_time', Time, latch=True)
+	tp = rospy.Publisher('~start_time', Time, queue_size=10, latch=True)
 	tp.publish(rospy.Time.now())
 
 	# Window duration
 	window_duration = rospy.get_param('~duration', 100)
-	wp = rospy.Publisher('~window_duration', Int32, latch=True)
+	wp = rospy.Publisher('~window_duration', Int32, queue_size=10, latch=True)
 	wp.publish(window_duration)
 
+	rospy.loginfo("Ready.")
 	rospy.spin()
 
 if __name__ == '__main__':
