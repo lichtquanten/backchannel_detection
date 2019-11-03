0af8593a9696e3f7552fd2ae1683a22867f0ac89
Sean Hackett <seanhackett55@gmail.com>
2019-11-03 18:00:43 -0500

    Fix prune scripts

--- prune_old_bags.py
+++ prune_old_bags.py
@@ -13,84 +13,84 @@ from std_msgs.msg import Time
 
 
 face_topics = [
-	'/camera1/face_info',
-	'/camera2/face_info',
-	'/camera3/face_info',
+    '/camera1/face_info',
+    '/camera2/face_info',
+    '/camera3/face_info',
 ]
 
 audio_topics = [
-	'/pid1/chunk',
-	'/pid2/chunk',
-	'/pid3/chunk',
+    '/pid1/chunk',
+    '/pid2/chunk',
+    '/pid3/chunk',
 ]
 
 def main(argv):
     in_bag = rosbag.Bag(argv[0], 'r')
     out_bag = rosbag.Bag(argv[1], 'w')
     with in_bag, out_bag:
-    	start_time = Time(rospy.Time(in_bag.get_start_time()))
-    	out_bag.write('/bc/start_time', start_time)
-    	for topic, msg, t in in_bag.read_messages(topics=face_topics + audio_topics):
-    		pid = re.findall(r'\d', topic)[0]
-    		if topic in face_topics:
-    			if not msg.landmarks:
-    				continue
+        start_time = Time(rospy.Time(in_bag.get_start_time()))
+        out_bag.write('/bc/start_time', start_time)
+        for topic, msg, t in in_bag.read_messages(topics=face_topics + audio_topics):
+            pid = re.findall(r'\d', topic)[0]
+            if topic in face_topics:
+                if not msg.landmarks:
+                    continue
 
-    			ff_msg = FaceFeatures()
-    			# Success
-    			ff_msg.success = True
+                ff_msg = FaceFeatures()
+                # Success
+                ff_msg.success = True
 
-    			# Landmarks
-    			# 2D landmarks not provided
+                # Landmarks
+                # 2D landmarks not provided
 
-    			# Landmarks 3D
-    			ff_msg.landmarks_3D = []
-    			for landmark in msg.landmarks:
-    				ff_msg.landmarks_3D.append(Vector3(landmark.x, landmark.y, landmark.z))
+                # Landmarks 3D
+                ff_msg.landmarks_3D = []
+                for landmark in msg.landmarks:
+                    ff_msg.landmarks_3D.append(Vector3(landmark.x, landmark.y, landmark.z))
 
-    			# Landmarks visibilities (unused)
-    			ff_msg.landmarks_visibilities = []
+                # Landmarks visibilities (unused)
+                ff_msg.landmarks_visibilities = []
 
-    			# Gaze angle
-    			ff_msg.gaze_angle = Point2D(msg.gazeAngle.x, msg.gazeAngle.y)
+                # Gaze angle
+                ff_msg.gaze_angle = Point2D(msg.gazeAngle.x, msg.gazeAngle.y)
 
-    			# Gaze left
-    			direction = msg.gazeDirection0
-    			ff_msg.gaze_left = Vector3(direction.x, direction.y, direction.z)
+                # Gaze left
+                direction = msg.gazeDirection0
+                ff_msg.gaze_left = Vector3(direction.x, direction.y, direction.z)
 
-    			# Gaze right
-    			direction = msg.gazeDirection1
-    			ff_msg.gaze_right = Vector3(direction.x, direction.y, direction.z)
+                # Gaze right
+                direction = msg.gazeDirection1
+                ff_msg.gaze_right = Vector3(direction.x, direction.y, direction.z)
 
-    			# Head pose
-    			pose = msg.head_pose
-    			point = Point(pose.x, pose.y, pose.z)
-    			pitch, yaw, roll = pose.rot_x, pose.rot_y, pose.rot_z
-    			x, y, z, w = quaternion_from_euler(roll, pitch, yaw)
-    			quaternion = Quaternion(x, y, z, w)
-    			ff_msg.head_pose = Pose(point, quaternion)
+                # Head pose
+                pose = msg.head_pose
+                point = Point(pose.x, pose.y, pose.z)
+                pitch, yaw, roll = pose.rot_x, pose.rot_y, pose.rot_z
+                x, y, z, w = quaternion_from_euler(roll, pitch, yaw)
+                quaternion = Quaternion(x, y, z, w)
+                ff_msg.head_pose = Pose(point, quaternion)
 
-    			# au name (unused)
-    			ff_msg.au_name = ''
+                # au name (unused)
+                ff_msg.au_name = ''
 
-    			# au intensity (unused)
-    			ff_msg.au_intensity = []
+                # au intensity (unused)
+                ff_msg.au_intensity = []
 
-    			array = FaceFeaturesArray()
-    			array.header = msg.header
-    			array.features = [ff_msg]
+                array = FaceFeaturesArray()
+                array.header = msg.header
+                array.features = [ff_msg]
 
-    			out_bag.write('/pid' + pid + '/face_features', array, t)
-    		elif topic in audio_topics:
-    			ad_msg = AudioData()
-    			ad_msg.header.stamp = msg.time
-    			ad_msg.data = msg.chunk
-    			ad_msg.sample_rate = 16000
-    			ad_msg.num_channels = 1
-    			ad_msg.sample_width = 2
-    			ad_msg.is_bigendian = True
+                out_bag.write('/pid' + pid + '/face_features', array, t)
+            elif topic in audio_topics:
+                ad_msg = AudioData()
+                ad_msg.header.stamp = msg.time
+                ad_msg.data = msg.chunk
+                ad_msg.sample_rate = 16000
+                ad_msg.num_channels = 1
+                ad_msg.sample_width = 2
+                ad_msg.is_bigendian = False
 
-    			out_bag.write('/pid' + pid + '/audio/data', ad_msg, t)
+                out_bag.write('/pid' + pid + '/audio/data', ad_msg, t)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
